978dc49f497437e6b8000d022aa2ac46051ccc84
depst0r <depstor90@gmail.com>
2021-02-10 13:28:45 +0300

    formData

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,26 @@ import React, { useState } from 'react'
 export const App = () => {
 
   const [disabled, setDisabled] = useState(true)
-  const [formDara, setFormData] = useState({
+  const [formData, setFormData] = useState({
     name: null,
     email: null,
     phone: null,
     password: null
   })
 
-  const sendDataForm = () => {
-    
+  const sendDataForm = data => {
+      fetch('https://form-a65b7-default-rtdb.firebaseio.com/dataForm.json', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-type': 'aplication/json'
+        }
+      })
+        .then(res => res.json())
+        .then(res => console.log(res))
   }
+
+  sendDataForm(formData)
 
   const handlerSubmitForm = event => {
     event.preventDefault()
@@ -21,9 +31,19 @@ export const App = () => {
   return<>
     <form onSubmit={handlerSubmitForm}>
     <div className="mb-3">
+      <label htmlFor="exampleInputName1" className="form-label">User Name</label>
+      <input type="text" className="form-control" id="exampleInputName1" aria-describedby="nameHelp" />
+      <div id="nameHelp" className="form-text"></div>
+    </div>
+    <div className="mb-3">
       <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
       <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
       <div id="emailHelp" className="form-text"></div>
+    </div>
+    <div className="mb-3">
+      <label htmlFor="exampleInputPhone1" className="form-label">Email address</label>
+      <input type="text" className="form-control" id="exampleInputPhone1" aria-describedby="phoneHelp" />
+      <div id="phoneHelp" className="form-text"></div>
     </div>
     <div className="mb-3">
       <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
